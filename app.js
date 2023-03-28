36c663d0d2a77458dcccb25ec454327f889e5508
furiyamvp <rixsiboyevyusuf87@gmail.com>
2023-03-28 17:04:14 +0500

    first coomit

---
+++ app.js
@@ -0,0 +1,77 @@
+
+///////////////////////////baho/////////////////////////////////
+
+// let baho = prompt()
+// alert(baho)
+// if (baho == 1 || baho == 0) {
+//     alert("juda yomon");
+// }
+// else if (baho == 2) {
+//     alert("yomon");
+// }
+// else if (baho == 3) {
+//     alert("qoniqarli");
+// }
+// else if (baho == 4) {
+//     alert("yaxshi");
+// }
+// else if (baho == 5) {
+//     alert("a'lo");
+// }
+// else {
+//     alert("bunday baho yoq")
+// }
+
+/////////////////////////////bo'y/////////////////////////////////
+
+// let boy = prompt()
+// alert(boy)
+// if (boy>=170 ) {
+//     alert(" qurolli kuchlarga hush kelibsiz");
+// }
+// else{
+//     alert("Mumkin emas")
+// }
+
+//////////////////////////Oylar///////////////////////////////
+
+// let kun = prompt()
+// alert(kun)
+// if (kun == 1)
+//     alert("Yanvar");
+
+//  else if (kun == 2)
+//     alert("Fevral");
+
+// else if (kun == 3)
+//     alert("mart");
+
+// else if (kun == 4)
+//     alert("aprel");
+
+// else if (kun == 5)
+//     alert("may");
+
+// else if (kun == 6)
+//     alert("iyun");
+
+// else if (kun == 7)
+//     alert("iyul");
+// else if (kun == 8)
+//     alert("avgust");
+
+// else if (kun == 9)
+//     alert("sentiabor");
+
+// else if (kun == 10)
+//     alert("oktyabor");
+
+// else if (kun == 11)
+//     alert("noyabor");
+
+// else if (kun == 12)
+//     alert("dekabor");
+// else {
+//     alert("bunday oy mavjud emas");
+// }
+/////////////////////////////////////////////////////////Tamom//////////////////////////////////////////////////////////
